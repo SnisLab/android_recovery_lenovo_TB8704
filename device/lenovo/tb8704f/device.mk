aae2d2f359adcddf284a85159be925f644063b1d
DjSni <31524614+DjSni@users.noreply.github.com>
2026-09-15 22:40:58 +0000

    recovery: include TWRP recovery package

--- device/lenovo/tb8704f/device.mk
+++ device/lenovo/tb8704f/device.mk
@@ -1,2 +1,5 @@
 # The kernel is consumed through TARGET_PREBUILT_KERNEL in BoardConfig.mk.
 # No legacy proprietary recovery runtime files are imported at this stage.
+
+PRODUCT_PACKAGES += \
+    recovery
