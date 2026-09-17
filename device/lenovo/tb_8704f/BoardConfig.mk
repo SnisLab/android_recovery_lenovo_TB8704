cbfeef55ee0bf2b01595599e255b1f62d84d403d
root <root@OpenCode2204.lo.snis.pw>
2026-09-17 15:58:33 +0000

    recovery: add Qualcomm hardware FDE support

--- device/lenovo/tb_8704f/BoardConfig.mk
+++ device/lenovo/tb_8704f/BoardConfig.mk
@@ -45,6 +45,5 @@ RECOVERY_SDCARD_ON_DATA := true
 TW_DEFAULT_EXTERNAL_STORAGE := true
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_INCLUDE_NTFS_3G := true
-# Crypto is intentionally disabled for the first recovery bring-up.
-TW_INCLUDE_CRYPTO := false
-TARGET_HW_DISK_ENCRYPTION := false
+TW_INCLUDE_CRYPTO := true
+TARGET_HW_DISK_ENCRYPTION := true
--- device/lenovo/tb_8704f/device.mk
+++ device/lenovo/tb_8704f/device.mk
@@ -1,2 +1,10 @@
 # The kernel is consumed through TARGET_PREBUILT_KERNEL in BoardConfig.mk.
-# No legacy proprietary recovery runtime files are imported at this stage.
+
+PRODUCT_COPY_FILES += \
+    device/lenovo/tb_8704f/recovery/root/init.recovery.qcom.rc:recovery/root/init.recovery.qcom.rc \
+    device/lenovo/tb_8704f/recovery/root/sbin/change_blockdev:recovery/root/sbin/change_blockdev \
+    device/lenovo/tb_8704f/recovery/root/sbin/qseecomd:recovery/root/sbin/qseecomd \
+    device/lenovo/tb_8704f/recovery/root/vendor/lib64/libQSEEComAPI.so:recovery/root/vendor/lib64/libQSEEComAPI.so \
+    device/lenovo/tb_8704f/recovery/root/vendor/lib64/libdiag.so:recovery/root/vendor/lib64/libdiag.so \
+    device/lenovo/tb_8704f/recovery/root/vendor/lib64/libdrmfs.so:recovery/root/vendor/lib64/libdrmfs.so \
+    device/lenovo/tb_8704f/recovery/root/vendor/lib64/hw/keystore.msm8953.so:recovery/root/vendor/lib64/hw/keystore.msm8953.so
